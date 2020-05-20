1603c71aba801abe40785a4b240a041fb4acaa4b
distillery-alexander-ageychenko <44116355+distillery-alexander-ageychenko@users.noreply.github.com>
2020-05-20 16:16:27 +0300

    Setup android project [AAG-1]  (#2)
    Create a new android project.

---
+++ app/src/main/java/com/distillery/android/blueprints/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.distillery.android.blueprints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
