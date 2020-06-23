a3434f031eaf5e895f8f3d2b4c15f69e268a810f
sasfmlzr <sasfmlzr@yandex.ru>
2020-06-23 16:35:12 +0300

    Remove BasePresenterProvider

--- mvp/src/main/java/com/distillery/android/blueprints/mvp/architecture/BasePresenterProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.distillery.android.blueprints.mvp.architecture
-
-/**
- * Presenter provider interface to create some presenters based on BasePresenter.
- * Needs because koin can't work with generic types.
- */
-interface BasePresenterProvider {
-    fun getPresenter(fragment: Presentable): BasePresenter
-}
