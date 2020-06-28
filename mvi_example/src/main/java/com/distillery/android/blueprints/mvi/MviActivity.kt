c40aa4d722be25d2eb13d3095f7eba403c0fce90
sebastian.tellez.o@gmail.com <sebastian.tellez.o@gmail.com>
2020-06-28 18:18:28 -0500

    Add UI fragment to Mvi example

--- mvi_example/src/main/java/com/distillery/android/blueprints/mvi/MviActivity.kt
+++ mvi_example/src/main/java/com/distillery/android/blueprints/mvi/MviActivity.kt
@@ -1,75 +1,20 @@
 package com.distillery.android.blueprints.mvi
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.distillery.android.blueprints.mvi.todo.TodoIntent
-import com.distillery.android.blueprints.mvi.todo.TodoListModel
-import com.distillery.android.blueprints.mvi.todo.state.ConfirmationCode
-import com.distillery.android.blueprints.mvi.todo.state.TodoState
-import com.distillery.android.blueprints.mvi.todo.viewmodel.TodoViewModel
+import com.distillery.android.blueprints.mvi.todo.fragments.TodoListFragment
+import com.distillery.android.ui.databinding.ActivityTodoBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @ExperimentalCoroutinesApi
 class MviActivity : AppCompatActivity() {
 
-    private val todoViewModel: TodoViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        configureObservers()
-        configureListeners()
-    }
-
-    private fun configureListeners() {
-        val button = Button(this)
-        button.setOnClickListener {
-            todoViewModel.proccessIntents(flow {
-                emit(TodoIntent.SaveTodo("Title", "Description"))
-            })
-        }
-    }
-
-    private fun configureObservers() {
-        todoViewModel.todoState
-                .onEach { state ->
-                    handleState(state)
-                }
-    }
-
-    private fun handleState(state: TodoState<TodoListModel>) {
-        when (state) {
-            is TodoState.LoadingState -> {
-                // show loader
-            }
-            is TodoState.DataState -> {
-                // hide loader
-                state.todoListFlow.completedTodoList // set to pending RecyclerView Adapter
-                state.todoListFlow.todoList // set to completed RecyclerView Adapter
-            }
-            is TodoState.ErrorState -> {
-                // hide loader
-                // show Error snackbar
-            }
-            is TodoState.ConfirmationState -> {
-                // hide loader
-                when (state.confirmationCode) {
-                    is ConfirmationCode.SAVED -> {
-                        // show snackbar
-                    }
-                    is ConfirmationCode.DELETED -> {
-                        // show snackbar
-                    }
-                    is ConfirmationCode.UPDATED -> {
-                        // show snackbar
-                    }
-                }
-            }
-        }
+        val binding = ActivityTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+                .replace(binding.container.id, TodoListFragment.instance())
+                .commit()
     }
 }
--- mvi_example/src/main/java/com/distillery/android/blueprints/mvi/todo/fragments/TodoListFragment.kt
+++ mvi_example/src/main/java/com/distillery/android/blueprints/mvi/todo/fragments/TodoListFragment.kt
@@ -1,11 +1,41 @@
 package com.distillery.android.blueprints.mvi.todo.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.distillery.android.blueprints.mvi.R
+import com.distillery.android.ui.databinding.FragmentTodoBinding
 
 class TodoListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var binding: FragmentTodoBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        binding.buttonAdd.setOnClickListener {
+            navigateToAddItemFragment()
+        }
+    }
+
+    private fun navigateToAddItemFragment() {
+        parentFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.container, AddTodoItemFragment.newInstance())
+                .commit()
+    }
+
+    companion object {
+        fun instance() = TodoListFragment()
     }
 }
